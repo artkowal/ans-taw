d2b5f0444beb69903ec34cd0e6672b93e2f81e51
Adrian Kowal <adrian2002kowal@gmail.com>
2025-01-28 23:05:47 +0100

    fix: commit local changes

---
+++ blog/.angular/cache/19.0.6/blog/vite/deps_ssr/@angular_ssr.js
@@ -0,0 +1,38 @@
+import { createRequire } from 'module';const require = createRequire(import.meta.url);
+import {
+  AngularAppEngine,
+  InlineCriticalCssProcessor,
+  PrerenderFallback,
+  RenderMode,
+  createRequestHandler,
+  destroyAngularServerApp,
+  extractRoutesAndCreateRouteTree,
+  getOrCreateAngularServerApp,
+  getRoutesFromAngularRouterConfig,
+  provideServerRoutesConfig,
+  setAngularAppEngineManifest,
+  setAngularAppManifest
+} from "./chunk-OQWZBWFK.js";
+import "./chunk-YPI3P3HQ.js";
+import "./chunk-3IKY5PQI.js";
+import "./chunk-LUWGNCKF.js";
+import "./chunk-6ZRQ553K.js";
+import "./chunk-7F2N3T4J.js";
+import "./chunk-AJOGVN3Y.js";
+import "./chunk-VKJO5GQP.js";
+import "./chunk-YHCV7DAQ.js";
+export {
+  AngularAppEngine,
+  PrerenderFallback,
+  RenderMode,
+  createRequestHandler,
+  provideServerRoutesConfig,
+  InlineCriticalCssProcessor as ɵInlineCriticalCssProcessor,
+  destroyAngularServerApp as ɵdestroyAngularServerApp,
+  extractRoutesAndCreateRouteTree as ɵextractRoutesAndCreateRouteTree,
+  getOrCreateAngularServerApp as ɵgetOrCreateAngularServerApp,
+  getRoutesFromAngularRouterConfig as ɵgetRoutesFromAngularRouterConfig,
+  setAngularAppEngineManifest as ɵsetAngularAppEngineManifest,
+  setAngularAppManifest as ɵsetAngularAppManifest
+};
+//# sourceMappingURL=@angular_ssr.js.map
